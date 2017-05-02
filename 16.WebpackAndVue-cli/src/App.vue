348d857317758f699e41db19e183dd763a543fcb
LeeBoy <jobboy19890101@gmail.com>
2017-05-02 19:22:21 +0800

    vuecli boilerplate

---
+++ 16.WebpackAndVue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template> 
+    <resource-view>
+      <img slot="logo" src="./assets/logo.png">
+    </resource-view>
+</template>
+
+<script>
+import resource from './components/resource.vue'
+export default {
+  name: 'Tutorial',
+  components: {
+    'resource-view': resource,
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
